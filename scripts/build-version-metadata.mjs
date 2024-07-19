7b9ad7778f13118116f6b3ba33bfab8e9a57bea7
Zach Shilton <4624598+zchsh@users.noreply.github.com>
2024-07-19 14:08:00 -0400

    add placeholder version props for parity

--- scripts/build-version-metadata.mjs
+++ scripts/build-version-metadata.mjs
@@ -5,22 +5,30 @@ const versionMetadata = {};
 
 const products = readdirSync(join("public", "products"));
 for (const product of products) {
-  versionMetadata[product] = [];
-  const versions = readdirSync(join("public", "products", product));
-  for (const version of versions) {
-    versionMetadata[product].push({ version });
-  }
+	versionMetadata[product] = [];
+	const versions = readdirSync(join("public", "products", product));
+	let idx = 0;
+	for (const version of versions) {
+		/**
+		 * TODO: implement meaningful releaseStage and isLatest.
+		 * Maybe like a `_version-metadata.json` in each version directory?
+		 */
+		const releaseStage = "stable";
+		const isLatest = idx === 0;
+		versionMetadata[product].push({ version, releaseStage, isLatest });
+		idx++;
+	}
 }
 
 writeFileSync(
-  join(
-    "app",
-    "api",
-    "content",
-    "[productSlug]",
-    "version-metadata",
-    "data.json"
-  ),
-  JSON.stringify(versionMetadata, null, 2),
-  "utf8"
+	join(
+		"app",
+		"api",
+		"content",
+		"[productSlug]",
+		"version-metadata",
+		"data.json"
+	),
+	JSON.stringify(versionMetadata, null, 2),
+	"utf8"
 );
