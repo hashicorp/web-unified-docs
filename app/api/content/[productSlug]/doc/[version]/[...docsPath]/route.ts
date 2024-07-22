414f8eaee3e022c1a388b073a5be466ae6561f41
Zach Shilton <4624598+zchsh@users.noreply.github.com>
2024-07-22 11:05:04 -0400

    fix docs content route with variable content dir

--- app/api/content/[productSlug]/doc/[version]/[...docsPath]/route.ts
+++ app/api/content/[productSlug]/doc/[version]/[...docsPath]/route.ts
@@ -10,16 +10,60 @@ export async function GET(
 ) {
 	const { productSlug, version, docsPath } = params;
 
+	/**
+	 * TODO: we have different content directory structures across repos.
+	 *
+	 * In the current API, powered by mktg-content-workflows, we have repo-config
+	 * that handles these differences. In the short term, it probably makes sense
+	 * to mirror that type of config in this repository.
+	 *
+	 * On the other hand, it would be nice to not have to deal with these cases...
+	 * Maybe during the process of migrating docs to this repo, we standardize
+	 * the directory structure? As long as the API routes are still drop-in
+	 * replacements for the previous API, maybe that'd make sense... rather than
+	 * migrating repo-config, we migrate in a way that requires less (maybe zero)
+	 * "repo-config".
+	 *
+	 * Related thought: migration scripting probably makes sense? Script would:
+	 * - Pull down the content source repo
+	 * - Iterate over known versions (based on our existing live content API), and
+	 *   for each known version of content...
+	 * - Check out the ref for that version
+	 * - Copy content into `public/products`, with the content directory now
+	 *   normalized (probably to `content`, since that's most common?)
+	 */
+	const contentDirMap: Record<string, string> = {
+		boundary: "content",
+		consul: "content",
+		"hcp-docs": "content",
+		nomad: "content",
+		packer: "content",
+		"ptfe-releases": "docs",
+		sentinel: "content",
+		terraform: "docs",
+		"terraform-cdk": "docs",
+		"terraform-docs-agents": "docs",
+		"terraform-docs-common": "docs",
+		"terraform-plugin-framework": "docs",
+		"terraform-plugin-log": "docs",
+		"terraform-plugin-mux": "docs",
+		"terraform-plugin-sdk": "docs",
+		"terraform-plugin-testing": "docs",
+		vagrant: "content",
+		vault: "content",
+		waypoint: "content",
+	};
+
+	// Determine the expected content path.
+	// Note that either an `.mdx` file or an `index.mdx` file may exist,
+	// we don't have a good way to determine which to expect, so we try both,
+	// and use the first one that comes back.
+	const contentPath = [contentDirMap[productSlug], ...docsPath].join("/");
+
 	const res = await Promise.all([
+		fetch(`${SELF_URL}/products/${productSlug}/${version}/${contentPath}.mdx`),
 		fetch(
-			`${SELF_URL}/products/${productSlug}/${version}/content/${docsPath.join(
-				"/"
-			)}.mdx`
-		),
-		fetch(
-			`${SELF_URL}/products/${productSlug}/${version}/content/${docsPath.join(
-				"/"
-			)}/index.mdx`
+			`${SELF_URL}/products/${productSlug}/${version}/${contentPath}/index.mdx`
 		),
 	]);
 
