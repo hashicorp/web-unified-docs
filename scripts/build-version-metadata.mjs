e49cd6e8ecd3856d2d388f4b5da5519818643473
Zach Shilton <4624598+zchsh@users.noreply.github.com>
2024-07-22 11:04:41 -0400

    update temporary isLatest script logic

--- scripts/build-version-metadata.mjs
+++ scripts/build-version-metadata.mjs
@@ -7,16 +7,14 @@ const products = readdirSync(join("public", "products"));
 for (const product of products) {
 	versionMetadata[product] = [];
 	const versions = readdirSync(join("public", "products", product));
-	let idx = 0;
-	for (const version of versions) {
+	for (const [idx, version] of versions.entries()) {
 		/**
 		 * TODO: implement meaningful releaseStage and isLatest.
 		 * Maybe like a `_version-metadata.json` in each version directory?
 		 */
 		const releaseStage = "stable";
-		const isLatest = idx === 0;
+		const isLatest = idx === versions.length - 1;
 		versionMetadata[product].push({ version, releaseStage, isLatest });
-		idx++;
 	}
 }
 
