9aa01716e590d93c4cf76e12fbcdc2b492c11b2b
Zach Shilton <4624598+zchsh@users.noreply.github.com>
2024-07-24 12:29:15 -0400

    iterate on placeholder build-version-metadata script

--- scripts/build-version-metadata.mjs
+++ scripts/build-version-metadata.mjs
@@ -1,21 +1,39 @@
 import { readdirSync, writeFileSync } from "fs";
 import { join } from "path";
+// Third-party
+import semver from "semver";
 
 const versionMetadata = {};
 
 const products = readdirSync(join("public", "products"));
 for (const product of products) {
 	versionMetadata[product] = [];
-	const versions = readdirSync(join("public", "products", product));
+	const rawVersions = readdirSync(join("public", "products", product));
+
+	// Semver sort versions
+	const versions = rawVersions
+		.filter((v) => {
+			return semver.valid(normalizeVersion(v));
+		})
+		.sort((a, b) => {
+			const [aVersion, bVersion] = [a, b].map(normalizeVersion);
+			return semver.compare(aVersion, bVersion);
+		})
+		.reverse();
+
 	for (const [idx, version] of versions.entries()) {
 		/**
 		 * TODO: implement meaningful releaseStage and isLatest.
 		 * Maybe like a `_version-metadata.json` in each version directory?
 		 */
 		const releaseStage = "stable";
-		const isLatest = idx === versions.length - 1;
+		const isLatest = idx === 0;
 		versionMetadata[product].push({ version, releaseStage, isLatest });
 	}
+}
+
+function normalizeVersion(version) {
+	return version.replace(/\.x$/, ".0");
 }
 
 writeFileSync(
