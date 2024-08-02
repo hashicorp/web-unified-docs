6a2fe0d8b195048e0318ab46268fca4eb5ad9881
Zach Shilton <4624598+zchsh@users.noreply.github.com>
2024-08-02 15:40:33 -0400

    start matching response shapes

--- app/api/content/[productSlug]/nav-data/[version]/[section]/route.ts
+++ app/api/content/[productSlug]/nav-data/[version]/[section]/route.ts
@@ -1,18 +1,20 @@
+const SELF_URL = process.env.VERCEL_URL || "http://localhost:3000";
+
 export async function GET(
-  request: Request,
-  {
-    params,
-  }: { params: { productSlug: string; version: string; section: string } }
+	request: Request,
+	{
+		params,
+	}: { params: { productSlug: string; version: string; section: string } }
 ) {
-  const { productSlug, version, section } = params;
+	const { productSlug, version, section } = params;
 
-  const res = await fetch(
-    `http://localhost:3000/products/${productSlug}/${version}/data/${section}-nav-data.json`
-  );
-  if (res.ok) {
-    const navData = await res.json();
-    return Response.json(navData);
-  }
+	const res = await fetch(
+		`${SELF_URL}/content/${productSlug}/${version}/data/${section}-nav-data.json`
+	);
+	if (res.ok) {
+		const navData = await res.json();
+		return Response.json({ result: navData });
+	}
 
-  return new Response("Not found", { status: 404 });
+	return new Response("Not found", { status: 404 });
 }
