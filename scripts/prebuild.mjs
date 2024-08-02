6baa8c8b5dfa5ad03f8f352f88264ed4751f9f7b
Zach Shilton <4624598+zchsh@users.noreply.github.com>
2024-08-02 15:40:07 -0400

    include gather-version-metadata in prebuild script

--- scripts/prebuild.mjs
+++ scripts/prebuild.mjs
@@ -1,5 +1,6 @@
 import path from "path";
 import buildMdxTransforms from "./mdx-transforms/build-mdx-transforms.mjs";
+import gatherVersionMetadata from "./gather-version-metadata.mjs";
 
 /**
  * We expect the current working directory to be the project root.
@@ -8,12 +9,21 @@ import buildMdxTransforms from "./mdx-transforms/build-mdx-transforms.mjs";
 const CWD = process.cwd();
 const CONTENT_DIR = path.join(CWD, "content");
 const CONTENT_DIR_OUT = path.join(CWD, "public", "content");
+const VERSION_METADATA_FILE = path.join(
+	CWD,
+	"app/api/content/[productSlug]/version-metadata/data.json"
+);
 
 /**
  * Define the prebuild script.
  */
 async function main() {
+	// Apply MDX transforms
 	await buildMdxTransforms(CONTENT_DIR, CONTENT_DIR_OUT);
+	// Gather and write out version metadata
+	const versionMetadata = await gatherVersionMetadata(CONTENT_DIR_OUT);
+	const versionMetadataJson = JSON.stringify(versionMetadata, null, 2);
+	fs.writeFileSync(VERSION_METADATA_FILE, versionMetadataJson);
 }
 
 /**
