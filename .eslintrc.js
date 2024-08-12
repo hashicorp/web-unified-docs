f9d55b6e9db0b700d6d3c8a9d7fb8d1855bcbe3b
Preston Bourne <preston.bourne@icloud.com>
2024-08-12 14:28:02 -0400

    chore: add hashicorp linting and formatting tools🧹

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+module.exports = {
+  root: true,
+  extends: "./node_modules/@hashicorp/platform-cli/config/.eslintrc.js",
+};
