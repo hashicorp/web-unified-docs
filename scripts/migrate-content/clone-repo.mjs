60431788ec2996db5713937b8a66ad231ca1a9c5
Zach Shilton <4624598+zchsh@users.noreply.github.com>
2024-08-27 11:54:02 -0400

    stub in content migration scripts (#18)
    * stub in content migration scripts
    
    * npm run format
    
    * add temp folders to gitignore
    
    * refactor to get release refs from content api
    
    * start cleaning up migration scripts
    
    * add TODO re version metadata
    
    * continue script cleanup
    
    * start on clone-all-repos script
    
    * continue stubbing in clone-repo scripting
    
    * test packer migration
    
    * continue cleanup
    
    * fix TODO comment
    
    * Fix formatting
    
    Co-authored-by: Robert Main <50675045+rmainwork@users.noreply.github.com>
    
    * Fix indentation
    
    Co-authored-by: Robert Main <50675045+rmainwork@users.noreply.github.com>
    
    * Fix typo
    
    Co-authored-by: Robert Main <50675045+rmainwork@users.noreply.github.com>
    
    * Fix missing word
    
    * rm unecessary fn
    
    * use URL object for content API URL construction
    
    Co-authored-by: Robert Main <50675045+rmainwork@users.noreply.github.com>
    
    * Update to avoid eslint-ignore
    
    Co-authored-by: Robert Main <50675045+rmainwork@users.noreply.github.com>
    
    * rm unused eslint-ignore comment
    
    * update use of new URL
    
    * add docs on cloneArgs
    
    * fix endpoint URL use
    
    * rm return statements, not necessary
    
    * add comment on exit behaviour
    
    * switch from gh cli to using git
    
    * remove unused async setup
    
    * rm logging of active handles
    
    * rm unecessary async
    
    ---------
    
    Co-authored-by: Robert Main <50675045+rmainwork@users.noreply.github.com>

---
+++ scripts/migrate-content/clone-repo.mjs
@@ -0,0 +1,46 @@
+import fs from 'fs'
+import path from 'path'
+import { execSync } from 'child_process'
+
+/**
+ * Clone the repository into the target directory, using the `gh` CLI.
+ *
+ * You must be authenticated, and have read access to the target repository,
+ * in order for this to work. We use a shallow clone since there are only a
+ * small percentage of refs with content we'll actually use. If the clone
+ * command fails, we'll throw an error, and we expect this to stop the script.
+ *
+ * Note that if the repository already exists, we do _not_ clone it again.
+ * It is therefore _possible_ the directory is a stale clone of the repo,
+ * or even something else entirely. We expect the consumer of this function
+ * to handle such scenarios, for example by starting from an empty directory.
+ *
+ * @param {string} targetDir The directory where the repository will be cloned.
+ * @param {string} repoOwner The owner of the repository.
+ * @param {string} repoSlug The name of the repository.
+ * @param {string} cloneArgs Additional arguments to pass to the `gh repo clone` command.
+ * @returns {string} The path to the repository directory.
+ */
+export function cloneRepo(targetDir, repoOwner, repoSlug, cloneArgs) {
+	/**
+	 * Assuming the `gh repo clone` command will be successful, we expect
+	 * the repository directory to be given the same name as the repository.
+	 */
+	const repoDir = path.join(targetDir, repoSlug)
+	const repoDirExists = fs.existsSync(repoDir)
+	// If the repository already exists, we skip cloning
+	if (!repoDirExists) {
+		console.log(`📡 Cloning "${repoSlug}" into "${targetDir}"...`)
+		execSync(
+			`git clone ${cloneArgs} https://github.com/${repoOwner}/${repoSlug}`,
+			{
+				stdio: 'inherit', // Nice to see progress for large repos
+				cwd: targetDir,
+			}
+		)
+	} else {
+		console.log(`🔄 Directory already exists at "${repoDir}". Skipping clone.`)
+	}
+	// Return the path to the previously-existing or newly-cloned directory
+	return repoDir
+}
