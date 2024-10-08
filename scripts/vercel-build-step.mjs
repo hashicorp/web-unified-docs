79547f26926cf457316bb05c3d8d9fb9df7a5a8e
Preston Bourne <preston.bourne@icloud.com>
2024-10-08 14:40:21 -0400

    use global args

--- scripts/vercel-build-step.mjs
+++ scripts/vercel-build-step.mjs
@@ -4,15 +4,52 @@ import { execSync } from 'child_process'
 // this file is executed by vercel
 // https://vercel.com/hashicorp/unified-docs-frontend-preview/settings/git
 
-console.log('[vercel-build-step.mjs]: executing...')
 const EXIT_CODES = {
 	BUILD: 1,
 	SKIP: 0,
 }
 
+// TODO: move this to a .env file
+const vercelToken = 'Oq1fRHagxUo1tAxc0GeCcDet'
+const vercelGlobalArgs = `--scope=hashicorp --token=${vercelToken}`
+
 const REPO_TO_CLONE = 'hashicorp/dev-portal'
 const PREVIEW_DIR = 'dev-portal-preview'
 
+const unifiedDocsPreviewUrl = process.env.VERCEL_BRANCH_URL
+if (!unifiedDocsPreviewUrl) {
+	console.error('VERCEL_BRANCH_URL environment variable not set.')
+	process.exit(EXIT_CODES.SKIP)
+}
+try {
+	cloneDevPortal()
+	process.chdir(PREVIEW_DIR)
+
+	process.env.IS_CONTENT_PREVIEW = 'true'
+	process.env.UNIFIED_DOCS_API = `https://${unifiedDocsPreviewUrl}/`
+
+	execSync('npm install')
+
+	ensureVercelIsInstalled()
+
+	execSync(`vercel pull --yes --environment=preview ${vercelGlobalArgs}`)
+	execSync(`vercel build ${vercelGlobalArgs}`)
+
+	const vercelCommand = `vercel deploy --prebuilt ${vercelGlobalArgs}`
+
+	const previewUrl = execSync(vercelCommand, { stdio: 'pipe' }).toString()
+	console.log(`Deploy Result: ${previewUrl}`)
+
+	console.log(
+		`Vercel Preview URL for branch "${unifiedDocsPreviewUrl}": ${previewUrl}`
+	)
+	process.exit(EXIT_CODES.BUILD)
+} catch (error) {
+	console.error('Error deploying dev-portal preview:', error)
+	process.exit(EXIT_CODES.BUILD)
+}
+
+// helper functions
 function cloneDevPortal() {
 	try {
 		console.log(`⏳ Cloning ${REPO_TO_CLONE}, this may take a while...`)
@@ -22,41 +59,15 @@ function cloneDevPortal() {
 		console.log(`✅ Cloned ${REPO_TO_CLONE}`)
 	} catch (error) {
 		console.error('Error cloning dev-portal repo:', error)
-		process.exit(EXIT_CODES.SKIP)
+		process.exit(EXIT_CODES.BUILD)
 	}
 }
 
-console.log(`env: ${JSON.stringify(process.env, null, 2)}`)
-
-const unifiedDocsPreviewUrl = process.env.VERCEL_BRANCH_URL
-if (!unifiedDocsPreviewUrl) {
-	console.error('VERCEL_BRANCH_URL environment variable not set.')
-	process.exit(EXIT_CODES.SKIP)
+function ensureVercelIsInstalled() {
+	try {
+		execSync('vercel --version', { stdio: 'ignore' })
+	} catch (error) {
+		console.log('Vercel CLI not found, installing...')
+		execSync('npm install -g vercel')
+	}
 }
-
-cloneDevPortal()
-process.chdir(PREVIEW_DIR)
-
-process.env.IS_CONTENT_PREVIEW = 'true'
-process.env.UNIFIED_DOCS_API = `https://${unifiedDocsPreviewUrl}/`
-
-execSync('npm install')
-
-try {
-	execSync('vercel --version', { stdio: 'ignore' })
-} catch (error) {
-	console.log('Vercel CLI not found, installing...')
-	execSync('npm install -g vercel')
-}
-execSync('vercel build')
-
-const vercelToken = 'Oq1fRHagxUo1tAxc0GeCcDet'
-const vercelCommand = `vercel deploy --scope=hashicorp --prebuilt --token=${vercelToken}`
-
-const previewUrl = execSync(vercelCommand, { stdio: 'pipe' }).toString()
-console.log(`Deploy Result: ${previewUrl}`)
-
-console.log(
-	`Vercel Preview URL for branch "${unifiedDocsPreviewUrl}": ${previewUrl}`
-)
-process.exit(EXIT_CODES.BUILD)
