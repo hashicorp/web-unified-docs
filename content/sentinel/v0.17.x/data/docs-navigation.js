96e271bea5ff878b03658a15a18df3a2ea157e49
Heat Hamilton <heat.hamilton@hashicorp.com>
2024-10-16 13:58:11 -0400

    Revert "Remove all content unrelated to terraform repos"
    This reverts commit 84a973ef9b40f81fc63854a32111909dddc64768.

---
+++ content/sentinel/v0.17.x/data/docs-navigation.js
@@ -0,0 +1,88 @@
+// The root folder for this documentation category is `pages/docs`
+//
+// - A string refers to the name of a file
+// - A "category" value refers to the name of a directory
+// - All directories must have an "index.mdx" file to serve as
+//   the landing page for the category
+
+export default [
+	'changelog',
+	{
+		category: 'concepts',
+		content: ['policy-as-code', 'language', 'imports', 'enforcement-levels'],
+	},
+	{
+		category: 'configuration',
+		content: ['remote-sources'],
+	},
+	{
+		category: 'commands',
+		content: ['apply', 'fmt', 'test'],
+	},
+	{
+		category: 'writing',
+		content: ['basic', 'rules', 'tracing', 'testing', 'imports', 'debugging'],
+	},
+	{
+		category: 'extending',
+		content: ['modules', 'plugins', 'internals'],
+	},
+	'----------------',
+	{
+		category: 'language',
+		content: [
+			'variables',
+			'values',
+			'lists',
+			'maps',
+			'rules',
+			'imports',
+			'parameters',
+			'boolexpr',
+			'arithmetic',
+			'slices',
+			'conditionals',
+			'loops',
+			'collection-operations',
+			'functions',
+			'scope',
+			'undefined',
+			'logging-errors',
+			'spec',
+		],
+	},
+	{
+		category: 'functions',
+		content: [
+			'append',
+			'delete',
+			'error',
+			'keys',
+			'length',
+			'print',
+			'range',
+			'values',
+		],
+	},
+	{
+		category: 'imports',
+		content: [
+			'base64',
+			'decimal',
+			'http',
+			'json',
+			'runtime',
+			'sockaddr',
+			'strings',
+			'time',
+			'types',
+			'units',
+			'version',
+		],
+	},
+	'----------------',
+	'consul',
+	'nomad',
+	'terraform',
+	'vault',
+]
---
+++ content/waypoint/v0.2.x/data/alert-banner.js
@@ -0,0 +1,7 @@
+export const ALERT_BANNER_ACTIVE = false
+
+export default {
+	tag: 'Announcing',
+	text: 'Some details here about the announcement',
+	url: 'https://hashicorp.com',
+}
