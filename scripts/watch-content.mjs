0ae41085bf7e352ef3372018ded88115ce3184d6
Ruben Nic <ruben.nic@hashicorp.com>
2024-10-29 15:29:24 -0400

    On partial change, transform files that use it

--- scripts/watch-content.mjs
+++ scripts/watch-content.mjs
@@ -7,11 +7,58 @@ const contentDir = path.resolve('content')
 fs.watch(contentDir, { recursive: true }, async (eventType, filename) => {
 	if (filename && path.extname(filename) === '.mdx') {
 		const filePath = path.join(contentDir, filename)
-		console.log(`File changed: ${filePath}`)
-		try {
-			await buildFileMdxTransforms(filePath)
-		} catch (error) {
-			console.error(`Error processing file ${filePath}:`, error)
+
+		if (filePath.includes('/partials/')) {
+			let currentDir = path.dirname(filePath)
+			let docsDir = null
+
+			console.log(`Partial file changed: ${filePath}`)
+
+			// Traverse up the directory tree to find the "docs" folder
+			while (currentDir !== path.resolve(currentDir, '..')) {
+				if (path.basename(currentDir) === 'docs') {
+					docsDir = currentDir
+					break
+				}
+				currentDir = path.resolve(currentDir, '..')
+			}
+
+			if (docsDir) {
+				const files = fs.readdirSync(docsDir, {
+					withFileTypes: true,
+					recursive: true,
+				})
+
+				// Find all MDX files that include the changed partial and transform them
+				for (const file of files) {
+					if (file.isFile() && path.extname(file.name) === '.mdx') {
+						const fullPath = path.join(file.path, file.name)
+
+						const fileContent = fs.readFileSync(fullPath, 'utf-8')
+
+						const partialName = `@include '${filePath.split('/partials/')[1]}'`
+						if (fileContent.includes(partialName)) {
+							try {
+								console.log(
+									`- File containing partial "${partialName}" changed: ${fullPath}\n`
+								)
+
+								await buildFileMdxTransforms(fullPath)
+							} catch (error) {
+								console.error(`Error processing file ${fullPath}:`, error)
+							}
+						}
+					}
+				}
+			}
+		} else {
+			console.log(`- File changed: ${filePath}\n`)
+
+			try {
+				await buildFileMdxTransforms(filePath)
+			} catch (error) {
+				console.error(`Error processing file ${filePath}:`, error)
+			}
 		}
 	}
 })
