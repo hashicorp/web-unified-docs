a8265446a3cec9e115d382e8b8900605dc7999cd
Ruben Nic <ruben.nic@hashicorp.com>
2024-10-31 15:27:21 -0400

    Attempt to get dev portal deployment vercel data

--- .github/actions/get-vercel-preview-url/dist/index.js
+++ .github/actions/get-vercel-preview-url/dist/index.js
@@ -31570,71 +31570,111 @@ __nccwpck_require__.r(__webpack_exports__);
 
 
 
-const VERCEL_TOKEN = _actions_core__WEBPACK_IMPORTED_MODULE_0__.getInput('vercel_token', { required: true })
+const DEVELOPMENT_TYPE = _actions_core__WEBPACK_IMPORTED_MODULE_0__.getInput('development_type', { required: true })
 const TEAM_ID = _actions_core__WEBPACK_IMPORTED_MODULE_0__.getInput('team_id', { required: true })
-const PROJECT_ID = _actions_core__WEBPACK_IMPORTED_MODULE_0__.getInput('project_id', { required: true })
-const GITHUB_REF_NAME = _actions_core__WEBPACK_IMPORTED_MODULE_0__.getInput('github_ref_name', { required: true })
+const VERCEL_TOKEN = _actions_core__WEBPACK_IMPORTED_MODULE_0__.getInput('vercel_token', { required: true })
 
-_actions_core__WEBPACK_IMPORTED_MODULE_0__.info(`Fetching Vercel preview URL for Unified Docs...`)
+// required by DEVELOPMENT_TYPE="url"
+const DEVELOPMENT_URL = _actions_core__WEBPACK_IMPORTED_MODULE_0__.getInput('development_url')
 
-node_fetch__WEBPACK_IMPORTED_MODULE_1___default()(
-	`https://api.vercel.com/v6/deployments?limit=1&projectId=${PROJECT_ID}&teamId=${TEAM_ID}`,
-	{
-		headers: {
-			Authorization: `Bearer ${VERCEL_TOKEN}`,
+// required by DEVELOPMENT_TYPE="unified-docs-api"
+const PROJECT_ID = _actions_core__WEBPACK_IMPORTED_MODULE_0__.getInput('project_id')
+const GITHUB_REF_NAME = _actions_core__WEBPACK_IMPORTED_MODULE_0__.getInput('github_ref_name')
+
+const processDeploymentData = (deploymentData) => {
+	const createdUnixTimeStamp = deploymentData.created
+	const createdDate = new Date(createdUnixTimeStamp)
+
+	const options = {
+		year: 'numeric',
+		month: 'short',
+		day: 'numeric',
+		hour: 'numeric',
+		minute: 'numeric',
+		hour12: true,
+	}
+	const formattedDate = createdDate.toLocaleString('en-US', options)
+
+	_actions_core__WEBPACK_IMPORTED_MODULE_0__.info(`Deployment created at (UTC): ${formattedDate}`)
+	_actions_core__WEBPACK_IMPORTED_MODULE_0__.setOutput('created_utc', formattedDate)
+
+	const previewUrl = `https://${deploymentData.url}`
+	_actions_core__WEBPACK_IMPORTED_MODULE_0__.info(`Vercel preview URL for Unified Docs: ${previewUrl}`)
+	_actions_core__WEBPACK_IMPORTED_MODULE_0__.setOutput('preview_url', previewUrl)
+
+	const inspectorUrl = deploymentData.inspectorUrl
+	_actions_core__WEBPACK_IMPORTED_MODULE_0__.info(`Vercel inspector URL for Unified Docs: ${inspectorUrl}`)
+	_actions_core__WEBPACK_IMPORTED_MODULE_0__.setOutput('inspector_url', inspectorUrl)
+}
+
+if (DEVELOPMENT_TYPE === 'url') {
+	_actions_core__WEBPACK_IMPORTED_MODULE_0__.info(`Fetching Vercel data for deployment url ${DEVELOPMENT_URL}...`)
+
+	node_fetch__WEBPACK_IMPORTED_MODULE_1___default()(
+		`https://api.vercel.com/v13/deployments/${DEVELOPMENT_URL}?teamId=${TEAM_ID}`,
+		{
+			headers: {
+				Authorization: `Bearer ${VERCEL_TOKEN}`,
+			},
 		},
-	},
-)
-	.then((res) => {
-		if (!res.ok) {
-			throw new Error(`HTTP error! Status: ${res.status}`)
-		}
-		return res.json()
-	})
-	.then((data) => {
-		if (data.deployments && data.deployments.length > 0) {
-			// Double check if the deployment is for the current ref
-			const deploymentData = data.deployments.find((deployment) => {
-				return deployment.meta.githubCommitRef === GITHUB_REF_NAME
-			})
+	)
+		.then((res) => {
+			if (!res.ok) {
+				throw new Error(`HTTP error! Status: ${res.status}`)
+			}
+			return res.json()
+		})
+		.then((deploymentData) => {
+			_actions_core__WEBPACK_IMPORTED_MODULE_0__.info(`Fetching Vercel preview URL for Unified Docs...`)
 
 			if (!deploymentData) {
 				throw new Error(`No deployment found for the ref: ${GITHUB_REF_NAME}`)
 			}
 
-			// const deploymentData = data.deployments[0]
+			processDeploymentData(deploymentData)
+		})
+		.catch((err) => {
+			_actions_core__WEBPACK_IMPORTED_MODULE_0__.error(err)
+			_actions_core__WEBPACK_IMPORTED_MODULE_0__.setFailed(`Failed to fetch Vercel preview URL.`)
+		})
+} else if (DEVELOPMENT_TYPE !== 'unified-docs-api') {
+	_actions_core__WEBPACK_IMPORTED_MODULE_0__.info(`Fetching Vercel preview URL for Unified Docs...`)
 
-			const createdUnixTimeStamp = deploymentData.created
-			const createdDate = new Date(createdUnixTimeStamp)
-
-			const options = {
-				year: 'numeric',
-				month: 'short',
-				day: 'numeric',
-				hour: 'numeric',
-				minute: 'numeric',
-				hour12: true,
+	node_fetch__WEBPACK_IMPORTED_MODULE_1___default()(
+		`https://api.vercel.com/v6/deployments?limit=5&projectId=${PROJECT_ID}&teamId=${TEAM_ID}`,
+		{
+			headers: {
+				Authorization: `Bearer ${VERCEL_TOKEN}`,
+			},
+		},
+	)
+		.then((res) => {
+			if (!res.ok) {
+				throw new Error(`HTTP error! Status: ${res.status}`)
 			}
-			const formattedDate = createdDate.toLocaleString('en-US', options)
+			return res.json()
+		})
+		.then((data) => {
+			if (data.deployments && data.deployments.length > 0) {
+				// Double check if the deployment is for the current ref
+				const deploymentData = data.deployments.find((deployment) => {
+					return deployment.meta.githubCommitRef === GITHUB_REF_NAME
+				})
 
-			_actions_core__WEBPACK_IMPORTED_MODULE_0__.info(`Deployment created at (UTC): ${formattedDate}`)
-			_actions_core__WEBPACK_IMPORTED_MODULE_0__.setOutput('created_utc', formattedDate)
+				if (!deploymentData) {
+					throw new Error(`No deployment found for the ref: ${GITHUB_REF_NAME}`)
+				}
 
-			const previewUrl = `https://${deploymentData.url}`
-			_actions_core__WEBPACK_IMPORTED_MODULE_0__.info(`Vercel preview URL for Unified Docs: ${previewUrl}`)
-			_actions_core__WEBPACK_IMPORTED_MODULE_0__.setOutput('preview_url', previewUrl)
-
-			const inspectorUrl = deploymentData.inspectorUrl
-			_actions_core__WEBPACK_IMPORTED_MODULE_0__.info(`Vercel inspector URL for Unified Docs: ${inspectorUrl}`)
-			_actions_core__WEBPACK_IMPORTED_MODULE_0__.setOutput('inspector_url', inspectorUrl)
-		} else {
-			throw new Error('No deployments found.')
-		}
-	})
-	.catch((err) => {
-		_actions_core__WEBPACK_IMPORTED_MODULE_0__.error(err)
-		_actions_core__WEBPACK_IMPORTED_MODULE_0__.setFailed(`Failed to fetch Vercel preview URL.`)
-	})
+				processDeploymentData(deploymentData)
+			} else {
+				throw new Error('No deployments found.')
+			}
+		})
+		.catch((err) => {
+			_actions_core__WEBPACK_IMPORTED_MODULE_0__.error(err)
+			_actions_core__WEBPACK_IMPORTED_MODULE_0__.setFailed(`Failed to fetch Vercel preview URL.`)
+		})
+}
 
 })();
 
--- .github/actions/get-vercel-preview-url/src/index.js
+++ .github/actions/get-vercel-preview-url/src/index.js
@@ -1,68 +1,108 @@
 import * as core from '@actions/core'
 import fetch from 'node-fetch'
 
-const VERCEL_TOKEN = core.getInput('vercel_token', { required: true })
+const DEVELOPMENT_TYPE = core.getInput('development_type', { required: true })
 const TEAM_ID = core.getInput('team_id', { required: true })
-const PROJECT_ID = core.getInput('project_id', { required: true })
-const GITHUB_REF_NAME = core.getInput('github_ref_name', { required: true })
+const VERCEL_TOKEN = core.getInput('vercel_token', { required: true })
 
-core.info(`Fetching Vercel preview URL for Unified Docs...`)
+// required by DEVELOPMENT_TYPE="url"
+const DEVELOPMENT_URL = core.getInput('development_url')
 
-fetch(
-	`https://api.vercel.com/v6/deployments?limit=1&projectId=${PROJECT_ID}&teamId=${TEAM_ID}`,
-	{
-		headers: {
-			Authorization: `Bearer ${VERCEL_TOKEN}`,
+// required by DEVELOPMENT_TYPE="unified-docs-api"
+const PROJECT_ID = core.getInput('project_id')
+const GITHUB_REF_NAME = core.getInput('github_ref_name')
+
+const processDeploymentData = (deploymentData) => {
+	const createdUnixTimeStamp = deploymentData.created
+	const createdDate = new Date(createdUnixTimeStamp)
+
+	const options = {
+		year: 'numeric',
+		month: 'short',
+		day: 'numeric',
+		hour: 'numeric',
+		minute: 'numeric',
+		hour12: true,
+	}
+	const formattedDate = createdDate.toLocaleString('en-US', options)
+
+	core.info(`Deployment created at (UTC): ${formattedDate}`)
+	core.setOutput('created_utc', formattedDate)
+
+	const previewUrl = `https://${deploymentData.url}`
+	core.info(`Vercel preview URL for Unified Docs: ${previewUrl}`)
+	core.setOutput('preview_url', previewUrl)
+
+	const inspectorUrl = deploymentData.inspectorUrl
+	core.info(`Vercel inspector URL for Unified Docs: ${inspectorUrl}`)
+	core.setOutput('inspector_url', inspectorUrl)
+}
+
+if (DEVELOPMENT_TYPE === 'url') {
+	core.info(`Fetching Vercel data for deployment url ${DEVELOPMENT_URL}...`)
+
+	fetch(
+		`https://api.vercel.com/v13/deployments/${DEVELOPMENT_URL}?teamId=${TEAM_ID}`,
+		{
+			headers: {
+				Authorization: `Bearer ${VERCEL_TOKEN}`,
+			},
 		},
-	},
-)
-	.then((res) => {
-		if (!res.ok) {
-			throw new Error(`HTTP error! Status: ${res.status}`)
-		}
-		return res.json()
-	})
-	.then((data) => {
-		if (data.deployments && data.deployments.length > 0) {
-			// Double check if the deployment is for the current ref
-			const deploymentData = data.deployments.find((deployment) => {
-				return deployment.meta.githubCommitRef === GITHUB_REF_NAME
-			})
+	)
+		.then((res) => {
+			if (!res.ok) {
+				throw new Error(`HTTP error! Status: ${res.status}`)
+			}
+			return res.json()
+		})
+		.then((deploymentData) => {
+			core.info(`Fetching Vercel preview URL for Unified Docs...`)
 
 			if (!deploymentData) {
 				throw new Error(`No deployment found for the ref: ${GITHUB_REF_NAME}`)
 			}
 
-			// const deploymentData = data.deployments[0]
+			processDeploymentData(deploymentData)
+		})
+		.catch((err) => {
+			core.error(err)
+			core.setFailed(`Failed to fetch Vercel preview URL.`)
+		})
+} else if (DEVELOPMENT_TYPE !== 'unified-docs-api') {
+	core.info(`Fetching Vercel preview URL for Unified Docs...`)
 
-			const createdUnixTimeStamp = deploymentData.created
-			const createdDate = new Date(createdUnixTimeStamp)
-
-			const options = {
-				year: 'numeric',
-				month: 'short',
-				day: 'numeric',
-				hour: 'numeric',
-				minute: 'numeric',
-				hour12: true,
+	fetch(
+		`https://api.vercel.com/v6/deployments?limit=5&projectId=${PROJECT_ID}&teamId=${TEAM_ID}`,
+		{
+			headers: {
+				Authorization: `Bearer ${VERCEL_TOKEN}`,
+			},
+		},
+	)
+		.then((res) => {
+			if (!res.ok) {
+				throw new Error(`HTTP error! Status: ${res.status}`)
 			}
-			const formattedDate = createdDate.toLocaleString('en-US', options)
+			return res.json()
+		})
+		.then((data) => {
+			if (data.deployments && data.deployments.length > 0) {
+				// Double check if the deployment is for the current ref
+				const deploymentData = data.deployments.find((deployment) => {
+					return deployment.meta.githubCommitRef === GITHUB_REF_NAME
+				})
 
-			core.info(`Deployment created at (UTC): ${formattedDate}`)
-			core.setOutput('created_utc', formattedDate)
+				if (!deploymentData) {
+					throw new Error(`No deployment found for the ref: ${GITHUB_REF_NAME}`)
+				}
 
-			const previewUrl = `https://${deploymentData.url}`
-			core.info(`Vercel preview URL for Unified Docs: ${previewUrl}`)
-			core.setOutput('preview_url', previewUrl)
-
-			const inspectorUrl = deploymentData.inspectorUrl
-			core.info(`Vercel inspector URL for Unified Docs: ${inspectorUrl}`)
-			core.setOutput('inspector_url', inspectorUrl)
-		} else {
-			throw new Error('No deployments found.')
-		}
-	})
-	.catch((err) => {
-		core.error(err)
-		core.setFailed(`Failed to fetch Vercel preview URL.`)
-	})
+				processDeploymentData(deploymentData)
+			} else {
+				throw new Error('No deployments found.')
+			}
+		})
+		.catch((err) => {
+			core.error(err)
+			core.setFailed(`Failed to fetch Vercel preview URL.`)
+		})
+}
