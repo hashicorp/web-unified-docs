9d39ec53a1f8109d184ed0a4c940f229a1d7781e
Leah Bush <leah.bush@hashicorp.com>
2025-01-06 14:06:47 -0600

    PR feedback: add another test

--- app/api/all-docs-paths/route.test.ts
+++ app/api/all-docs-paths/route.test.ts
@@ -7,7 +7,7 @@ afterEach(() => {
 	vi.restoreAllMocks()
 })
 
-test('GET should return a 200 response for happy path', async () => {
+test('GET should return a 200 response with no products', async () => {
 	vi.spyOn(getDocsPaths, 'getDocsPaths').mockResolvedValueOnce({
 		ok: true,
 		value: Object.values(docsPathsMock).flat(),
@@ -22,7 +22,7 @@ test('GET should return a 200 response for happy path', async () => {
 	expect(response.status).toBe(200)
 })
 
-test('GET should return a 200 response for happy path with products in the search params', async () => {
+test('GET should return a 200 response for one product in the search params', async () => {
 	vi.spyOn(getDocsPaths, 'getDocsPaths').mockResolvedValueOnce({
 		ok: true,
 		value: docsPathsMock.terraform,
@@ -32,6 +32,26 @@ test('GET should return a 200 response for happy path with products in the searc
 	}
 	const request = mockRequest(
 		`http://localhost:8080/api/all-docs-paths?products=terraform`,
+	)
+
+	const response = await GET(request)
+
+	expect(response.status).toBe(200)
+})
+
+test('GET should return a 200 response for multiple products in the search params', async () => {
+	vi.spyOn(getDocsPaths, 'getDocsPaths').mockResolvedValueOnce({
+		ok: true,
+		value: [
+			...docsPathsMock.terraform,
+			...docsPathsMock['terraform-docs-common'],
+		],
+	})
+	const mockRequest = (url: string) => {
+		return new Request(url)
+	}
+	const request = mockRequest(
+		`http://localhost:8080/api/all-docs-paths?products=terraform&products=terraform-docs-common`,
 	)
 
 	const response = await GET(request)
