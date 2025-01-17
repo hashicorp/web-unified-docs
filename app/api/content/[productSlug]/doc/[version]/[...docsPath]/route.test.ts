12d7480a557a04a15be66dcc6ff7e651b3cc2738
Robert Main <50675045+rmainwork@users.noreply.github.com>
2025-01-17 15:56:28 -0500

    Mock out contentVersions for better test isolation
    This helps ensure that the tests aren't relying on code that isn't part
    of the unit under test (in this case, the API route handler)

--- app/api/content/[productSlug]/doc/[version]/[...docsPath]/route.test.ts
+++ app/api/content/[productSlug]/doc/[version]/[...docsPath]/route.test.ts
@@ -1,6 +1,16 @@
 import { expect, describe, it, vi, beforeEach } from 'vitest'
 import { GET, GetParams } from './route'
 import { PRODUCT_CONFIG } from '@utils/productConfig.mjs'
+import { Err } from '@utils/result'
+import { getProductVersion } from '@utils/contentVersions'
+
+vi.mock(import('@utils/contentVersions'), async (importOriginal) => {
+	const mod = await importOriginal() // type is inferred
+	return {
+		...mod,
+		getProductVersion: vi.fn(),
+	}
+})
 
 // Keep the mapped types up here, so that things don't become a mess of
 // TypeScript spaghetti
@@ -27,13 +37,18 @@ describe('GET /[productSlug]/[version]/[...docsPath]', () => {
 		await expect(response.text()).resolves.toMatch(/not found/i)
 	})
 	it('returns a 404 for nonexistent versions', async () => {
+		// Real product name
+		const productSlug = Object.keys(PRODUCT_CONFIG)[0]
+
+		// Some junk data for version
+		const version = 'lorem ipsum dolor sit amet'
+		vi.mocked(getProductVersion).mockReturnValue(
+			Err(`Product, ${productSlug}, has no "${version}" version`),
+		)
 		const response = await mockRequest('', {
 			docsPath: [''],
-			// Real product name
-			productSlug: Object.keys(PRODUCT_CONFIG)[0],
-
-			// Some junk data for version
-			version: 'lorem ipsum dolor sit amet',
+			productSlug,
+			version,
 		})
 
 		expect(response.status).toBe(404)
