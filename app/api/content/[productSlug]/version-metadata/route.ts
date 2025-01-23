7d9dc34fa446498ad553d31193a30e2fbe31f7ea
Robert Main <50675045+rmainwork@users.noreply.github.com>
2025-01-23 15:00:36 -0500

    Add tests for version metadata endpoint (#104)
    * Add OK and Error Tests
    
    Also add a TypeScript type to capture the function parameters in the
    route handler
    
    * Tweak generated URL to better match the real URL structure
    
    Similar to changes made in 6931dcd923b34a0f3a64a9ae487639d84ec885f2

--- app/api/content/[productSlug]/version-metadata/route.ts
+++ app/api/content/[productSlug]/version-metadata/route.ts
@@ -1,10 +1,17 @@
 import { getProductVersionMetadata } from '@utils/contentVersions'
 import { errorResultToString } from '@utils/result'
 
-export async function GET(
-	request: Request,
-	{ params }: { params: { productSlug: string } },
-) {
+/**
+ * Parameters expected by `GET` route handler
+ */
+export type GetParams = {
+	/**
+	 * The product that version metadata is being requested for (i.e "terraform")
+	 */
+	productSlug: string
+}
+
+export async function GET(request: Request, { params }: { params: GetParams }) {
 	const { productSlug } = params
 
 	const productVersionMetadataResult = getProductVersionMetadata(productSlug)
