be4065c37b3539bef97edb607b83bc0d1b72b4bf
Heat Hamilton <heat.hamilton@hashicorp.com>
2025-01-27 19:28:15 -0500

    Added tests for getVersionFromFilePath and added logic to handle versionless docs

--- scripts/utils/file-path/version/index.mjs
+++ scripts/utils/file-path/version/index.mjs
@@ -1,3 +1,15 @@
+import semver from 'semver'
+/**
+ * Extracts the version from a given file path.
+ *
+ * This function parses the file path to extract the version information
+ * based on predefined regular expressions. It supports both general product
+ * versions and Terraform Enterprise versions.
+ *
+ * @param {string} filePath - The file path from which to extract the version.
+ * @returns {string} The extracted version from the file path.
+ * @throws {Error} If the file path is empty.
+ */
 export function getVersionFromFilePath(filePath) {
 	if (!filePath.length) {
 		throw new Error('File path is empty')
@@ -16,5 +28,13 @@ export function getVersionFromFilePath(filePath) {
 		TFE_VERSION_IN_PATH_REGEX.test(filePath)
 	) {
 		return version
+	} else {
+		/**
+		 * returns null if the version is not in the expected format
+		 * this is expected if the file path is for versionless docs
+		 * e.g. terraform-docs-common
+		 */
+
+		return semver.valid(semver.coerce(version))
 	}
 }
--- scripts/utils/file-path/version/index.test.mjs
+++ scripts/utils/file-path/version/index.test.mjs
@@ -1,82 +1,38 @@
 import { describe, test, expect } from 'vitest'
-import { isLatestVersion } from '../latest-version/index.mjs'
-import versionMetadata from '../../../../__fixtures__/versionMetadata.json'
-import {
-	getLatestVersionFromFilePath,
-	getVersionFromFilePath,
-} from './get-version-from-file-path.mjs'
+import { getVersionFromFilePath } from '../version/index.mjs'
 
-describe('isLatestVersion', () => {
-	test.only('should return undefined when file path is empty', () => {
-		const result = isLatestVersion('', versionMetadata)
-		expect(result).toBeUndefined()
+describe('getVersionFromFilePath', () => {
+	test('should throw an error if file path is empty', () => {
+		expect(() => {
+			getVersionFromFilePath('')
+		}).toThrow('File path is empty')
 	})
 
-	test('should return true when filePath is valid and version is latest', () => {
-		const result = isLatestVersion(
-			'public/content/terraform/v1.9.x/docs/intro/core-workflow.mdx',
-			versionMetadata,
-		)
-		expect(result).toBe(true)
+	test('should return the correct version for a product version in the file path', () => {
+		const filePath =
+			'content/terraform-plugin-mux/v0.13.5/docs/plugin/mux/index.mdx'
+		const version = getVersionFromFilePath(filePath)
+		expect(version).toBe('v0.13.5')
 	})
 
-	test('should return false when version is not the latest', () => {
-		const result = isLatestVersion(
-			'public/content/ptfe-releases/v202207-2/docs/enterprise/run/api.mdx',
-			versionMetadata,
-		)
-		expect(result).toBe(false)
+	test('should return the correct version for a product version with .x in the file path', () => {
+		const filePath =
+			'content/terraform-plugin-mux/v0.13.x/docs/plugin/mux/index.mdx'
+		const version = getVersionFromFilePath(filePath)
+		expect(version).toBe('v0.13.x')
 	})
 
-	test('should return true when file is in versionless directory', () => {
-		const result = isLatestVersion(
-			'public/content/terraform-docs-common/docs/docs/partnerships.mdx',
-			versionMetadata,
-		)
-		expect(result).toBe(true)
+	test('should return the correct version for a Terraform Enterprise version in the file path', () => {
+		const filePath =
+			'content/ptfe-releases/v202410-1/docs/enterprise/some-doc.mdx'
+		const version = getVersionFromFilePath(filePath)
+		expect(version).toBe('v202410-1')
 	})
 
-	describe('getLatestVersionFromFilePath', () => {
-		test('should return undefined when file path is empty', () => {
-			const result = getLatestVersionFromFilePath('', versionMetadata)
-			expect(result).toBeUndefined()
-		})
-
-		test('should return the latest version for a versioned product', () => {
-			const result = getLatestVersionFromFilePath(
-				'public/content/terraform/v1.9.x/docs/intro/core-workflow.mdx',
-				versionMetadata,
-			)
-			expect(result).toBe('1.9.0')
-		})
-
-		test('should return null when the file path is for a versionless product', () => {
-			const result = getLatestVersionFromFilePath(
-				'public/content/terraform-docs-common/docs/docs/partnerships.mdx',
-				versionMetadata,
-			)
-			expect(result).toBeNull()
-		})
-
-		describe('getVersionFromFilePath', () => {
-			test('should return undefined when file path is empty', () => {
-				const result = getVersionFromFilePath('')
-				expect(result).toBeUndefined()
-			})
-
-			test('should return valid semver version when file path contains version', () => {
-				const result = getVersionFromFilePath(
-					'public/content/terraform/v1.9.x/docs/intro/core-workflow.mdx',
-				)
-				expect(result).toBe('1.9.0')
-			})
-
-			test('should return null when file path does not contain a valid version', () => {
-				const result = getVersionFromFilePath(
-					'public/content/terraform-docs-common/docs/docs/partnerships.mdx',
-				)
-				expect(result).toBeNull()
-			})
-		})
+	test('should return null if the file path is for versionless docs', () => {
+		const filePath =
+			'content/terraform-docs-common/docs/cloud-docs/cost-estimation/gcp.mdx'
+		const version = getVersionFromFilePath(filePath)
+		expect(version).toBeNull()
 	})
 })
