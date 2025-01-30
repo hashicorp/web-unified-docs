471d567175417fefea9773a7c3ed7e73e5b56a2a
Heat Hamilton <heat.hamilton@hashicorp.com>
2025-01-30 13:45:30 -0500

    Move getLatestProductVersionDirectories to util folder

--- scripts/algolia/build-algolia-records.mjs
+++ scripts/algolia/build-algolia-records.mjs
@@ -3,74 +3,62 @@ import grayMatter from 'gray-matter'
 import { createAlgoliaRecordObject } from './transform-mdx-to-algolia-record/create-records.mjs'
 import { listFiles } from '../utils/list-files.mjs'
 import { batchPromises } from '../utils/batch-promises.mjs'
-import { getLatestVersion } from '../utils/file-path/latest-version/index.mjs'
-import { readdir, readFile, writeFile } from 'node:fs/promises'
+import { readFile, writeFile } from 'node:fs/promises'
+import { getLatestProductVersionDirectories } from './convert-mdx-to-json/index.mjs'
 
-async function getLatestProductVersionDirectories(dir, versionMetadata) {
-	// Gather all sub-directories from the specified directory
-	const directories = await readdir(dir, { withFileTypes: true })
-
-	// Iterate over product directories and return the list of latest version directories
-	const productDirectories = await Promise.all(
-		directories.map(async (directory) => {
-			const directoryPath = path.join(dir, directory.name)
-			const latestProductVersion = getLatestVersion(
-				directoryPath,
-				versionMetadata,
-			)
-			// latestProductVersion will return null for versionless product
-			if (latestProductVersion == null) {
-				return directoryPath
-			} else {
-				return path.join(directoryPath, latestProductVersion)
-			}
-		}),
-	)
-	return productDirectories
-}
-
-export async function buildAlgoliaRecords(targetDir, versionMetadata) {
+/**
+ * Retrieves all MDX files from the latest product version directories.
+ *
+ * @param {string} targetDir - The target directory to search for MDX files.
+ * @param {Object} versionMetadata - Metadata containing version information.
+ * @returns {Promise<string[]>} A promise that resolves to an array of MDX file paths.
+ */
+async function getAllMdxFiles(targetDir, versionMetadata) {
 	// get latest product directories, returns array
 	const latestProductVersionDirectories =
 		await getLatestProductVersionDirectories(targetDir, versionMetadata)
 
-	const allFilesPromises = latestProductVersionDirectories.map((dir) => {
+	const latestFilesPromises = latestProductVersionDirectories.map((dir) => {
 		return listFiles(dir)
 	})
 
-	const mdxFiles = (await Promise.all(allFilesPromises))
-		.flat()
-		.filter((filePath) => {
-			return (
-				path.extname(filePath) === '.mdx' && !filePath.includes('_template')
-			)
-		})
+	return (await Promise.all(latestFilesPromises)).flat().filter((filePath) => {
+		return (
+			// only include mdx files that are not templates or partials
+			path.extname(filePath) === '.mdx' &&
+			!filePath.includes('_template') &&
+			!filePath.includes('/partials/')
+		)
+	})
+}
 
-	// Filter for .mdx files
-	// const mdxFiles = allFiles.filter(
-	// 	(filePath) => path.extname(filePath) === '.mdx',
-	// )
+/**
+ * Builds Algolia records from the latest product version directories.
+ *
+ * @param {string} targetDir - The target directory containing product versions.
+ * @param {Object} versionMetadata - Metadata about the product versions.
+ * @returns {Promise<void>} - A promise that resolves when the Algolia records have been built.
+ *
+ * @throws {Error} - Throws an error if there is an issue reading files or creating Algolia records.
+ */
+export async function buildAlgoliaRecords(targetDir, versionMetadata) {
+	const mdxFiles = await getAllMdxFiles(targetDir, versionMetadata)
 
 	console.log(
 		`🪄 Converting ${mdxFiles.length} files to JSON for the Algolia search index...`,
 	)
-	const batchSize = 32
-
 	const results = await batchPromises(
 		mdxFiles,
 		async (entry) => {
 			try {
-				const fileString = await readFile(entry).then((mdx) => {
-					return mdx
-				})
-				// get mdx content and frontmatter
-				const { data, content } = grayMatter(fileString)
-				return createAlgoliaRecordObject(content, data, entry)
+				const mdx = await readFile(entry)
+				const { data, content } = grayMatter(mdx)
+				return await createAlgoliaRecordObject(content, data, entry)
 			} catch (err) {
 				throw new Error(err)
 			}
 		},
-		batchSize,
+		64,
 	)
 
 	const ALGOLIA_RECORDS_JSON_PATH = path.join(
@@ -80,13 +68,14 @@ export async function buildAlgoliaRecords(targetDir, versionMetadata) {
 
 	const stringifiedResults = JSON.stringify(results, null, 2)
 	try {
-		// fs.writeFileSync(ALGOLIA_RECORDS_JSON_PATH, stringifiedResults)
 		writeFile(ALGOLIA_RECORDS_JSON_PATH, stringifiedResults)
+		console.log(
+			`✅ Converted ${mdxFiles.length} files to JSON for the Algolia search index.`,
+		)
 	} catch (error) {
+		console.error(
+			`❌ Failed to convert mdx files to JSON for the Algolia search index due to ${error}`,
+		)
 		throw new Error(error)
 	}
-	// Log out that the script has complete
-	console.log(
-		`✅ Converted ${mdxFiles.length} files to JSON for the Algolia search index.`,
-	)
 }
