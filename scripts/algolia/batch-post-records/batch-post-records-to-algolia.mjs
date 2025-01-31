815813d8631f2fdb6ce028a41f8228a08b479227
Heat Hamilton <heat.hamilton@hashicorp.com>
2025-01-31 17:15:36 -0500

    Removed env vars; added indices and API keys to vercel project settings

--- scripts/algolia/batch-post-records/batch-post-records-to-algolia.mjs
+++ scripts/algolia/batch-post-records/batch-post-records-to-algolia.mjs
@@ -7,7 +7,7 @@ export async function batchPostRecords(searchObjectsFile) {
 		process.env.ALGOLIA_API_KEY,
 	)
 	const searchIndex = searchClient.initIndex(process.env.ALGOLIA_INDEX_NAME)
-	console.log({ searchIndex })
+
 	const data = fs.readFileSync(searchObjectsFile, 'utf-8')
 	const searchObjects = JSON.parse(data)
 
@@ -16,12 +16,9 @@ export async function batchPostRecords(searchObjectsFile) {
 		console.log(
 			`🚧 Saving ${searchObjects.length} objects to the ${process.env.ALGOLIA_INDEX_NAME} Algolia index...`,
 		)
-		// await searchIndex.replaceAllObjects(searchObjects, { safe: true })
+		await searchIndex.replaceAllObjects(searchObjects, { safe: true })
 		console.log(
 			`✅ Completed saving objects to the ${process.env.ALGOLIA_INDEX_NAME} Algolia index.`,
-		)
-		console.log(
-			`target env: ${process.env.VERCEL_TARGET_ENV},\n production url: ${process.env.VERCEL_PROJECT_PRODUCTION_URL},\n VERCEL_BRANCH_URL: ${process.env.VERCEL_BRANCH_URL}`,
 		)
 	} catch (e) {
 		throw new Error(`Failed to save objects: ${e}`)
