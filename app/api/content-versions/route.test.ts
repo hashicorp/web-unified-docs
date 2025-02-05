c445203afc5e9f23f0772807f93bce838e74b7e7
Ruben Nic <RubenSandwich@users.noreply.github.com>
2025-02-05 10:14:01 -0500

    Mock files for content versions test (#116)
    * Mock files for content versions test
    
    * have the file mock get project dir

--- app/api/content-versions/route.test.ts
+++ app/api/content-versions/route.test.ts
@@ -1,5 +1,21 @@
-import { expect, test, vi } from 'vitest'
+import { expect, test, vi, beforeEach, afterEach } from 'vitest'
 import { GET } from './route'
+
+import { vol } from 'memfs'
+
+// Mock fs module
+vi.mock('node:fs')
+vi.mock('node:fs/promises')
+
+beforeEach(() => {
+	// Reset the state of in-memory fs
+	vol.reset()
+})
+
+afterEach(() => {
+	// Reset all mocks after each test
+	vi.resetAllMocks()
+})
 
 test('should return 400 if `product` query parameter is missing', async () => {
 	const mockRequest = (url: string) => {
@@ -31,40 +47,9 @@ test('should return 400 if `fullPath` query parameter is missing', async () => {
 	)
 })
 
-test('should return versions if `product` and `fullPath` query parameters are valid', async () => {
-	vi.spyOn(console, 'error').mockImplementation(() => {}) // suppress console.error
-	const mockedResponse = {
-		versions: [
-			'v202401-1',
-			'v202401-2',
-			'v202402-1',
-			'v202402-2',
-			'v202404-1',
-			'v202404-2',
-			'v202405-1',
-			'v202406-1',
-			'v202407-1',
-			'v202408-1',
-			'v202409-1',
-			'v202409-2',
-			'v202409-3',
-			'v202410-1',
-		],
-	}
-	const mockRequest = (url: string) => {
-		return new Request(url)
-	}
-	const request = mockRequest(
-		`http://localhost:8080/api/content-versions?product=ptfe-releases&fullPath=doc%23enterprise%2Freleases%2F2024%2Fv202401-1`,
-	)
-	const response = await GET(request)
-	expect(response.status).toBe(200)
-	const json = await response.json()
-	expect(json).toEqual(mockedResponse)
-	vi.resetAllMocks()
-})
-
 test('should return 200 and empty array if no content exists for the query params', async () => {
+	vol.fromJSON({})
+
 	const mockRequest = (url: string) => {
 		return new Request(url)
 	}
@@ -77,81 +62,69 @@ test('should return 200 and empty array if no content exists for the query param
 	expect(json).toEqual({ versions: [] })
 })
 
-test('should return 200 and array of strings', async () => {
+test('should return 200 and array of strings on valid params', async () => {
+	vol.fromJSON({
+		[`${process.cwd()}/content/terraform-cdk/v0.19.x/data/cd-ktf-nav-data.json`]:
+			JSON.stringify([
+				{
+					title: 'API Reference',
+					routes: [
+						{
+							title: 'Go',
+							routes: [
+								{
+									title: 'Overview',
+									path: 'api-reference/go',
+								},
+							],
+						},
+					],
+				},
+			]),
+		[`${process.cwd()}/content/terraform-cdk/v0.20.x/data/cd-ktf-nav-data.json`]:
+			JSON.stringify([
+				{
+					title: 'API Reference',
+					routes: [
+						{
+							title: 'Python',
+							routes: [
+								{
+									title: 'Overview',
+									path: 'api-reference/python',
+								},
+							],
+						},
+					],
+				},
+			]),
+		[`${process.cwd()}/content/terraform-cdk/v0.21.x/data/cd-ktf-nav-data.json`]:
+			JSON.stringify([
+				{
+					title: 'API Reference',
+					routes: [
+						{
+							title: 'Python',
+							routes: [
+								{
+									title: 'Overview',
+									path: 'api-reference/python',
+								},
+							],
+						},
+					],
+				},
+			]),
+	})
+
 	const mockedResponse = {
-		versions: [
-			'v0.12.x',
-			'v0.13.x',
-			'v0.14.x',
-			'v0.15.x',
-			'v0.16.x',
-			'v0.17.x',
-			'v0.18.x',
-			'v0.19.x',
-			'v0.20.x',
-		],
+		versions: ['v0.20.x', 'v0.21.x'],
 	}
 	const mockRequest = (url: string) => {
 		return new Request(url)
 	}
 	const request = mockRequest(
 		`http://localhost:8080/api/content-versions?product=terraform-cdk&fullPath=doc%23cdktf%2Fapi-reference%2Fpython`,
-	)
-	const response = await GET(request)
-	expect(response.status).toBe(200)
-	const json = await response.json()
-	expect(json).toEqual(mockedResponse)
-})
-
-/**
- * This repo is out of sync with the ptfe-releases repo,
- * so the endpoint below only returns two versions.
- * The current Content API returns this response:
- * {
- *  "versions": [
- *    "v202401-1",
- *    "v202401-2",
- *    "v202410-1",
- *    "v202402-1",
- *    "v202402-2",
- *    "v202404-1",
- *    "v202404-2",
- *    "v202405-1",
- *    "v202406-1",
- *    "v202407-1",
- *    "v202408-1",
- *    "v202409-1",
- *    "v202409-2",
- *    "v202409-3"
- *   ]
- * }
- * If this test fails in the future,
- * the fix may be updating mockedResponse
- */
-test('should return 200 and array of strings for nav data', async () => {
-	const mockedResponse = {
-		versions: [
-			'v202401-1',
-			'v202401-2',
-			'v202402-1',
-			'v202402-2',
-			'v202404-1',
-			'v202404-2',
-			'v202405-1',
-			'v202406-1',
-			'v202407-1',
-			'v202408-1',
-			'v202409-1',
-			'v202409-2',
-			'v202409-3',
-			'v202410-1',
-		],
-	}
-	const mockRequest = (url: string) => {
-		return new Request(url)
-	}
-	const request = mockRequest(
-		`http://localhost:8080/api/content-versions?product=ptfe-releases&fullPath=doc%23enterprise%2Freleases%2F2024%2Fv202401-1`,
 	)
 	const response = await GET(request)
 	expect(response.status).toBe(200)
