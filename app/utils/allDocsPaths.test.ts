bcefde5f3e2f329378d1bd754d2748798302acbb
Leah Bush <leah.bush@hashicorp.com>
2025-02-28 10:21:02 -0600

    feat: use docsPathsAllVersions instead of docsPaths

--- app/utils/allDocsPaths.test.ts
+++ app/utils/allDocsPaths.test.ts
@@ -1,33 +1,37 @@
 import { expect, test, vi } from 'vitest'
 import { getDocsPaths } from './allDocsPaths'
-import docsPathsMock from '../../__fixtures__/docsPaths.json'
+import docsPathsMock from '../../__fixtures__/docsPathsAllVersionsMock.json'
 
-test('getDocsPaths should return all docs paths for an empty productSlugs array', async () => {
+test('getDocsPaths should return an error for an empty productSlugs array', async () => {
 	const response = await getDocsPaths([], docsPathsMock)
-	expect(response).toEqual({
-		ok: true,
-		value: Object.values(docsPathsMock).flat(),
-	})
+	expect(response).toEqual({ ok: false, value: 'All docs paths not found' })
 })
 
 test('getDocsPaths should return an error if there are no paths for an empty productSlugs array', async () => {
+	// @ts-expect-error - Testing error case
 	const response = await getDocsPaths([], {})
 	expect(response).toEqual({ ok: false, value: 'All docs paths not found' })
 })
 
 test('getDocsPaths should return filtered docs paths when a non-empty productSlugs array is provided', async () => {
-	const response = await getDocsPaths(['terraform'], docsPathsMock)
-	const mockValue = Object.values(docsPathsMock['terraform']).flat()
+	const response = await getDocsPaths(
+		['terraform-plugin-framework'],
+		docsPathsMock,
+	)
+	const mockValue = Object.values(
+		docsPathsMock['terraform-plugin-framework']['v1.13.x'],
+	).flat()
 	expect(response).toEqual({ ok: true, value: mockValue })
 })
 
 test('getDocsPaths should return an error if there are no paths for a non-empty productSlugs array', async () => {
 	const mockConsole = vi.spyOn(console, 'error').mockImplementation(() => {})
 
-	const response = await getDocsPaths(['terraform'], {})
+	// @ts-expect-error - Testing error case
+	const response = await getDocsPaths(['terraform-plugin-framework'], {})
 	expect(mockConsole).toHaveBeenCalledOnce()
 	expect(mockConsole).toHaveBeenLastCalledWith(
-		'Product, terraform, not found in docs paths',
+		'Product, terraform-plugin-framework, not found in docs paths',
 	)
 	expect(response).toEqual({ ok: false, value: 'All docs paths not found' })
 })
