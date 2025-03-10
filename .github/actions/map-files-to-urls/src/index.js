dde642f01fbfa413ba59f8f771b8ef64a10507cf
Leah Bush <leah.bush@hashicorp.com>
2025-03-10 10:35:42 -0500

    verify sha

--- .github/actions/map-files-to-urls/src/index.js
+++ .github/actions/map-files-to-urls/src/index.js
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: BUSL-1.1
+ */
+
 import { getUrlFromFilePath } from '../../../../scripts/utils/file-path/url/index.mjs'
 import * as core from '@actions/core'
 
