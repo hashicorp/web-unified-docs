9c24ed5622fd58760ee23a4ddc3da94a449ba133
Leah Bush <leah.bush@hashicorp.com>
2025-03-10 15:31:31 -0500

    update dist

--- .github/actions/map-files-to-urls/dist/index.js
+++ .github/actions/map-files-to-urls/dist/index.js
@@ -30718,9 +30718,9 @@ var core = __nccwpck_require__(7484);
 const FILE_PATHS = core.getInput('files', { required: true })
 
 const getAllPaths = () => {
-    console.log('### file paths', FILE_PATHS)
+	console.log('### file paths', FILE_PATHS)
 	const pathsArray = FILE_PATHS.split(',')
-    console.log('### pathsArray', pathsArray)
+	console.log('### pathsArray', pathsArray)
 	const result = pathsArray.map((filePath) => {
 		try {
 			const url = getUrlFromFilePath(filePath)
@@ -30729,7 +30729,7 @@ const getAllPaths = () => {
 			core.error(error)
 		}
 	})
-    console.log('### result', result)
+	console.log('### result', result)
 	core.setOutput('paths', result)
 }
 
