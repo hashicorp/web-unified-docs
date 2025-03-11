6e1948133efe0575a545093e1c1721d0315c5f7e
Leah Bush <leah.bush@hashicorp.com>
2025-03-11 13:52:29 -0500

    change script

---
+++ scripts/map-files-to-urls.mjs
@@ -0,0 +1,26 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: BUSL-1.1
+ */
+
+import { getUrlFromFilePath } from './utils/file-path/url/index.mjs'
+
+const filePaths = process.argv.slice(2)
+
+// Run the main script
+main(filePaths)
+
+function main(filePaths) {
+	const pathsArray = filePaths.split(' ')
+	const result = pathsArray.map((filePath) => {
+		try {
+			const url = getUrlFromFilePath(filePath)
+			console.log(`URL for ${filePath}: ${url}`)
+			return url
+		} catch (error) {
+			console.error(error)
+		}
+	})
+
+	return result
+}
