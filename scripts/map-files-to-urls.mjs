dfb1fdf5393058843d1089823f842248c5ad8064
Leah Bush <leah.bush@hashicorp.com>
2025-03-12 16:08:37 -0500

    feat: add isr github action

---
+++ scripts/map-files-to-urls.mjs
@@ -0,0 +1,24 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: BUSL-1.1
+ */
+
+import { getUrlFromFilePath } from './utils/file-path/url/index.mjs'
+
+const filePaths = process.argv.slice(2)
+
+// Run the main script
+main(filePaths)
+
+function main(filePaths) {
+	const result = filePaths.map((filePath) => {
+		try {
+			const url = getUrlFromFilePath(filePath)
+			return url
+		} catch (error) {
+			console.error(error)
+		}
+	})
+
+	process.stdout.write(JSON.stringify(result))
+}
