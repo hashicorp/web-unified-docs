5abeeedcad761268edbddffd435516080018e46b
Ruben Nic <ruben.nic@hashicorp.com>
2025-05-21 13:16:28 -0400

    Test the whole workflow connected together

--- .github/actions/copy-cloud-docs-for-tfe/index.ts
+++ .github/actions/copy-cloud-docs-for-tfe/index.ts
@@ -1,10 +1,10 @@
-// import * as core from '@actions/core'
+import * as core from '@actions/core'
 
 import { main } from './main'
 
 async function action() {
-	// const newTFEVersion = core.getInput('new_TFE_version')
-	const newTFEVersion = 'v000011-1'
+	const newTFEVersion = core.getInput('new_TFE_version')
+	// const newTFEVersion = 'v000011-1'
 
 	await main(newTFEVersion)
 }
