e270fe283661e96e9934d0934c2a7056e419aba6
Ruben Nic <ruben.nic@hashicorp.com>
2025-06-13 15:29:13 -0400

    Add v2 of the /content-versions api

---
+++ app/api/v2/content-versions/route.ts
@@ -0,0 +1,59 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: BUSL-1.1
+ */
+import { PRODUCT_CONFIG } from '@utils/productConfig.mjs'
+import docsPathsAllVersions from '../../docsPathsAllVersions.json'
+
+export async function GET(request: Request) {
+	const url = new URL(request.url)
+	let product = url.searchParams.get('product')
+	const id = url.searchParams.get('id')
+
+	if (product === 'ptfe-releases') {
+		product = 'terraform-enterprise'
+	}
+
+	// If a `product` parameter has not been provided, return a 400
+	if (!product) {
+		return new Response(
+			'Missing `product` query parameter. Please provide the `product` under which the requested document is expected to be found, for example `vault`.',
+			{ status: 400 },
+		)
+	}
+	// If a `fullPath` parameter has not been provided, return a 400
+	if (!id) {
+		return new Response(
+			'Missing `id` query parameter.',
+			{ status: 400 },
+		)
+	}
+
+	if (!Object.keys(PRODUCT_CONFIG).includes(product)) {
+		console.error(`API Error: Product, ${product}, not found in contentDirMap`)
+		return new Response('Not found', { status: 404 })
+	}
+
+	// Import the docsPathsAllVersions.json
+
+	// Get all versions for the given product
+	const productVersions = docsPathsAllVersions[product] || {}
+
+	// Find versions where the id exists in the docs
+	const versions = Object.entries(productVersions)
+		.filter(([version, docs]) => {
+			// docs is an array of doc objects with at least an 'id' and 'path'
+			return Array.isArray(docs) && docs.some((doc: any) => doc.id === id)
+		})
+		.map(([version, docs]) => {
+			const doc = (docs as any[]).find((doc) => doc.id === id)
+			return {
+				version,
+				path: doc?.path,
+			}
+		})
+
+	return Response.json({
+		versions,
+	})
+}
