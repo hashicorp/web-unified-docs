f1d0ee99a1cd81c73ae7896b677ec8b36d02f615
Sarah Chavis <62406755+schavis@users.noreply.github.com>
2025-11-21 16:56:22 -0800

    test file update

--- scripts/sync-ga-to-rc/test.sh
+++ scripts/sync-ga-to-rc/test.sh
@@ -1,5 +1,5 @@
 currDir="$(dirname "$0")"
-. "${currDir}/definitions.sh"
+. "${currDir}/bash-helpers/definitions.sh"
 
 dateString=$(git reflog --date=iso origin/boundary/0.21.0   | tail -1)
 
@@ -7,6 +7,7 @@ snip1="${dateString%%\}*}"
 rawDate="${snip1#*{}"
 branchDate="$(getUTCDate "${rawDate}")"
 
+echo "$(echo $0)"
 echo "Date string: ${dateString}"
 echo "Snip 1: ${snip1}"
 echo "Raw date: ${rawDate}"
