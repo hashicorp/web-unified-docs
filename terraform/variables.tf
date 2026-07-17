339c271136339b7e95d7f937f123442191da410e
Robert Main <50675045+rmainwork@users.noreply.github.com>
2026-07-17 16:51:19 -0400

    Remove unused terraform variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -16,36 +16,6 @@ variable "slack_alerting_channel_id" {
   default     = "d921s4i0j8og0p7ogbeg"
 }
 
-variable "granularity_minutes" {
-  description = "Evaluation granularity in minutes."
-  type        = number
-  default     = 5
-}
-
-variable "rum_alert_grace_period_minutes" {
-  description = "Cooldown/grace period specified in minutes"
-  type        = number
-  default     = 5
-}
-
-variable "time_window_minutes" {
-  description = "Violation sequence time window in minutes."
-  type        = number
-  default     = 5
-}
-
-variable "warning_threshold" {
-  description = "Warning threshold for JS error count. Use 1 to mimic Datadog y > 0 behavior."
-  type        = number
-  default     = 1
-}
-
-variable "critical_threshold" {
-  description = "Critical threshold for JS error count."
-  type        = number
-  default     = 2
-}
-
 variable "instana_agent_key" {
   description = "Agent key used to submit metrics to Instana. Obtained from: https://prod-hashicorp.instana.io/#/instanaagent/installation"
   type        = string
@@ -56,7 +26,6 @@ variable "instana_otlp_endpoint" {
   description = "Instana OTLP endpoint for sending metrics to Instana."
   type        = string
 }
-
 
 variable "instana_application_violation_time_window_minutes" {
   description = "Violation sequence time window in minutes for application alerting."
@@ -69,13 +38,6 @@ variable "instana_repo_sync_failed_warning_threshold" {
   type        = number
   default     = 1
 }
-
-variable "instana_content_not_found_warning_threshold" {
-  description = "Warning threshold for content-not-found application alert throughput."
-  type        = number
-  default     = 500
-}
-
 
 variable "github_repository" {
   description = "Canonical name of the GitHub repository to manage."
