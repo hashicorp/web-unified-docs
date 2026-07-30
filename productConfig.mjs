4f5e5486c877f2728ac44c07caa0074b27861045
Robin Norwood <rnorwood@hashicorp.com>
2026-07-30 17:07:31 -0500

    Initial tfctl docs set

--- productConfig.mjs
+++ productConfig.mjs
@@ -387,6 +387,18 @@ export const PRODUCT_CONFIG = {
 		 * `terraform-plugin-mux/website/data: No such file or directory`
 		 * This likely indicates that older versions of docs have a different
 		 * directory structure that needs to be accounted for. Need to confirm.
+	'tfctl-cli': {
+		assetDir: 'img',
+		basePaths: ['tfctl-cli'],
+		contentDir: 'docs',
+		dataDir: 'data',
+		navDataPath: 'tfctl-cli',
+		productSlug: 'terraform',
+		semverCoerce: semver.coerce,
+		versionedDocs: true,
+		websiteDir: 'website',
+	},
+
 		 * See note at top of this document on `pages` directories for details.
 		 */
 		assetDir: 'img',
