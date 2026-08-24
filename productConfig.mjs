a926dd98f9f03916be2afe911890d5a517ca7226
William Dalessandro <will.dalessandro@hashicorp.com>
2026-08-23 21:04:23 -0400

    rename folders

--- productConfig.mjs
+++ productConfig.mjs
@@ -118,7 +118,7 @@ export const PRODUCT_CONFIG = {
 		versionedDocs: false,
 		websiteDir: '.',
 	},
-	'hcp-vault-radar': {
+	'vault-radar-hcp': {
 		assetDir: 'img',
 		contentDir: 'docs',
 		dataDir: 'data',
